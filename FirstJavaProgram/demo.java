249378c60e3ea9b028f7bb4a9600cf158ee1311b
Vansh1419 <vanshjain1419@gmail.com>
2022-02-18 00:03:01 +0530

    vid7 21:00

---
+++ FirstJavaProgram/demo.java
@@ -0,0 +1,17 @@
+package FirstJavaProgram;
+
+public class demo {
+    // ❤️public is used here so that this class colud be access anywhere
+    // ❤️class is a name group of properties and functions
+    public static void main(String[] args) {
+        /*
+         * ❤️ main function is the entry point of java file
+         * ❤️ public makes main function to be available to be executed from any where
+         * ❤️ static is used here as main function is the entry point and static is used
+         * to execute main function without creating a object of main class
+         * ❤️ String[]args--> it is command line argument
+         */
+        System.out.println("hello world");
+
+    }
+}

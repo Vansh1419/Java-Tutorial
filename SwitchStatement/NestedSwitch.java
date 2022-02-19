c6a8c1e613febfdf0a28f90806c226f9fb947d19
Vansh1419 <vanshjain1419@gmail.com>
2022-02-19 23:32:59 +0530

    nestedSwitch modified

--- SwitchStatement/NestedSwitch.java
+++ SwitchStatement/NestedSwitch.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class NestedSwitch {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int empId=in.nextInt();
-        String department=in.next();
+        Scanner in = new Scanner(System.in);
+        int empId = in.nextInt();
+        String department = in.next();
         switch (empId) {
             case 1:
                 System.out.println("kunal");
                 break;
-                case 2:
+            case 2:
                 System.out.println("vansh");
-                
+
                 break;
             case 3:
                 switch (department) {
@@ -26,14 +26,14 @@ public class NestedSwitch {
                     case "executive":
                         System.out.println("executive");
                         break;
-                
+
                     default:
                         break;
                 }
                 break;
-        
+
             default:
-            System.out.println("enter correct Id");
+                System.out.println("enter correct Id");
                 break;
         }
     }
